d1f5ef07f685ef14c78389af01dd858ace53d95d
Lihui Guo <lguo16@u.rochester.edu>
2018-09-16 17:05:43 -0400

    php includes

--- contributions.php
+++ contributions.php
@@ -1,12 +1,4 @@
-<!doctype html>
-<html lang=en>
-  <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>Dimitar Dobrev</title>
-
-      <link rel="stylesheet" href="css/styles.css" >
-    </head>
+<?php include 'inc/html-top.inc'; ?>
 
     <body>
       <h1>contributions</h1>
